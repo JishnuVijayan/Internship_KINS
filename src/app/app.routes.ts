f2d905905d28f1d46b4616a1d529f4edc2d30795
JishnuVijayan <jishnu03ptb@gmail.com>
2024-12-16 11:26:03 +0530

    Routes to Home and Details component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { NameDetailsComponent } from './components/name-details/name-details.component';
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'details',
+    component: NameDetailsComponent,
+  },
+];
